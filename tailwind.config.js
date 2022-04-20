73e28fc2b4c6a11375b54ee58e42468f57530ea2
Arash Ghelman <arashghelman77@gmail.com>
2022-04-20 18:08:26 +0430

    fix: configure 1px border width and Nunito Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+      fontFamily: {
+        nunito: ["'Nunito Sans'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
