0a918796cf04ac2c3f074d5dffbd6b5826fab5a7
Arash Ghelman <arashghelman77@gmail.com>
2022-04-20 18:13:02 +0430

    feat(impl): create mockData file
    a mockData.js file is created to mock the app's data.

---
+++ src/utils/mockData.js
@@ -0,0 +1,16 @@
+export const quiz = {
+  id: "1",
+  name: "Basic English Test - 1",
+  status: { type: "Public" },
+  subjects: [
+    { name: "English", id: "1" },
+    { name: "World Languages", id: "2" },
+    { name: "Fun", id: "3" },
+  ],
+  grades: {
+    min: { name: "1st", id: "1" },
+    max: { name: "9th", id: "2" },
+  },
+  description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam esse velit quibusdam, libero iure quaerat eius, illum maxime rem quae aperiam accusantium, ipsum fugiat beatae eos autem? Quia, explicabo repellendus.`,
+  thumbnail: "https://blog.hubspot.com/hubfs/google-quiz.jpg",
+};
