bf10ddffa35489173e019ab4051b7674a8cc4e59
Arash Ghelman <arashghelman77@gmail.com>
2022-04-25 11:59:39 +0430

    feat(impl): create QuestionsList story file

---
+++ src/pages/quizEditor/QuestionsList.stories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import QuestionsList from "./QuestionsList";
+
+export default {
+  title: "QuizEditor/QuestionsList",
+  component: QuestionsList,
+};
+
+const Template = () => <QuestionsList />;
+
+export const Default = Template.bind({});
