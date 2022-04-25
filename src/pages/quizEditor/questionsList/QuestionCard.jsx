801a15667fdc1de7d7ea47a8bb294943f5cebbdd
Arash Ghelman <arashghelman77@gmail.com>
2022-04-25 14:58:38 +0430

    feat(impl): define state and handler for collapse

--- src/pages/quizEditor/questionsList/QuestionCard.jsx
+++ src/pages/quizEditor/questionsList/QuestionCard.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./questionCard/Header";
 import ActionButton from "./questionCard/ActionButton";
+import CollapseButton from "./questionCard/CollapseButton";
 import EditIcon from "remixicon-react/PencilLineIcon";
 import DuplicateIcon from "remixicon-react/FileCopyLineIcon";
 import DeleteIcon from "remixicon-react/DeleteBinLineIcon";
@@ -10,20 +11,28 @@ export default function QuestionCard({
   config: { color, icon, actions },
   data: { id, title, number, optionsList },
 }) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const handleCollapse = () => setIsCollapsed((prev) => !prev);
+
   return (
     <div className="flex">
       <MoveBar color={color} />
-      <div className="flex flex-col gap-4 border-t-1 border-b-1 border-r-1 rounded-r-lg left-2 px-6 py-4 w-full">
-        <div className="flex justify-between items-start">
-          <Header icon={icon} color={color} data={{ title, number }} />
-          <div className="flex gap-2">
-            {actions}
-            <ActionButton icon={<DeleteIcon className="w-sm" />} />
-            <ActionButton icon={<DuplicateIcon className="w-sm" />} />
-            <ActionButton icon={<EditIcon className="w-sm" />} />
+      <div className="border-t-1 border-b-1 border-r-1 rounded-r-lg left-2 w-full">
+        <div className="flex flex-col gap-5 px-6 py-4">
+          <div className="flex justify-between items-start">
+            <Header icon={icon} color={color} data={{ title, number }} />
+            <div className="flex gap-2">
+              {actions}
+              <ActionButton icon={<DeleteIcon className="w-sm" />} />
+              <ActionButton icon={<DuplicateIcon className="w-sm" />} />
+              <ActionButton icon={<EditIcon className="w-sm" />} />
+            </div>
           </div>
+          {!isCollapsed && (
+            <ul className="flex flex-col gap-3">{optionsList}</ul>
+          )}
         </div>
-        <ul className="flex flex-col gap-3">{optionsList}</ul>
+        <CollapseButton isCollapsed={isCollapsed} onCollapse={handleCollapse} />
       </div>
     </div>
   );
