30a86dc8b8a08e3b5bc006302bc7dbfdb4766792
Arash Ghelman <arashghelman77@gmail.com>
2022-04-25 11:41:19 +0430

    fix: add custom configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      textColor: {
+        primary: "#374151",
+        lighter: "#4b5563",
+      },
+      width: {
+        base: "1.3rem",
+        sm: "1.1rem",
+      },
       borderWidth: {
         1: "1px",
       },
