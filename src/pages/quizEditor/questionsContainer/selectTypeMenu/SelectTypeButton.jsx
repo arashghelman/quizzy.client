4761f7c0837207368776a1941a6bc0fa1fa4f68a
Arash Ghelman <arashghelman77@gmail.com>
2022-06-02 12:18:26 +0430

    feat(impl): create SelectTypeButton component file

---
+++ src/pages/quizEditor/questionsContainer/selectTypeMenu/SelectTypeButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@/components/Button";
+import QuestionIcon from "@/components/QuestionIcon";
+
+export default function SelectTypeButton({ type, label }) {
+  return (
+    <Button
+      variant="p-base font-semibold w-full justify-between items-center 
+      border-b-1 last:border-b-0 hover:bg-gray-light-3"
+    >
+      {label}
+      <QuestionIcon type={type} size="w-xs" />
+    </Button>
+  );
+}
