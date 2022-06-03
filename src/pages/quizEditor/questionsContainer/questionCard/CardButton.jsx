c435aa560629ebacde6a63d3c550724a99da78ff
Arash Ghelman <arashghelman77@gmail.com>
2022-06-03 16:47:01 +0430

    feat(impl): define title prop for CardButton component

--- src/pages/quizEditor/questionsContainer/questionCard/CardButton.jsx
+++ src/pages/quizEditor/questionsContainer/questionCard/CardButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Button from "@/components/Button";
 
-export default function CardButton({ icon, onClick }) {
+export default function CardButton({ title, icon, onClick }) {
   return (
     <Button
+      title={title}
       variant="justify-center text-gray-400 hover:bg-gray-light-3 w-8 h-8 rounded"
       onClick={onClick}
     >
