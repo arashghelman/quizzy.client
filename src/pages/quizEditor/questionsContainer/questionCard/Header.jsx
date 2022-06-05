4684b6aee21db075df22a9a5cfa227d3c4886de5
Arash Ghelman <arashghelman77@gmail.com>
2022-06-05 12:33:01 +0430

    fix: remove icon container

--- src/pages/quizEditor/questionsContainer/questionCard/Header.jsx
+++ src/pages/quizEditor/questionsContainer/questionCard/Header.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-export default function Header({ icon, color, data: { title, number } }) {
+export default function Header({ icon, data: { title, number } }) {
   return (
     <div className="flex items-center text-gray-base gap-base">
-      <div
-        className={`${color} flex justify-center items-center rounded w-12 h-12`}
-      >
-        {icon}
-      </div>
+      {icon}
       <div className="flex flex-col gap-extra-tight">
         <span className="text-sm">Question {number}</span>
         <h1 className="text-lg font-semibold">Q. {title}</h1>
