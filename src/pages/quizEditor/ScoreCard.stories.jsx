8cc853b291be8b2b68818f36ed23bb3f8692233e
Arash Ghelman <arashghelman77@gmail.com>
2022-06-05 12:44:29 +0430

    feat(impl): define a default value for ScoreCard story

--- src/pages/quizEditor/ScoreCard.stories.jsx
+++ src/pages/quizEditor/ScoreCard.stories.jsx
@@ -4,16 +4,20 @@ import ScoreCard from "./ScoreCard";
 export default {
   title: "QuizEditor/ScoreCard",
   component: ScoreCard,
+  argTypes: {
+    quizState: {
+      defaultValue: {
+        hasName: true,
+        hasGrades: true,
+        hasDescription: true,
+        hasQuestions: true,
+        isPublic: true,
+      },
+      control: "object",
+    },
+  },
 };
 
 const Template = (args) => <ScoreCard {...args} />;
+
 export const Default = Template.bind({});
-Default.args = {
-  quizState: {
-    hasName: true,
-    hasGrades: true,
-    hasDescription: true,
-    hasQuestions: true,
-    isPublic: true,
-  },
-};
