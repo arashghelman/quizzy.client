6a24276e7d53f7975b47b3533db85c1f798571b8
Arash Ghelman <arashghelman77@gmail.com>
2022-06-05 12:17:58 +0430

    feat(impl): define a sizeConfig object

--- src/components/QuestionIcon.jsx
+++ src/components/QuestionIcon.jsx
@@ -1,25 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 import MultipleChoiceIcon from "remixicon-react/CheckboxMultipleLineIcon";
 import PollIcon from "remixicon-react/ChatPollLineIcon";
 import { questionType } from "@/constants/questionType";
 
-export default function QuestionIcon({ type, size = "w-base" }) {
-  let config = {
+export default function QuestionIcon({ type, size = "base" }) {
+  let sizeConfig = {
+    iconSize: "",
+    width: "",
+    height: "",
+  };
+  switch (size) {
+    case "base":
+      sizeConfig = { iconSize: "w-base", width: "w-12", height: "h-12" };
+      break;
+
+    case "sm":
+      sizeConfig = { iconSize: "w-xs", width: "w-7", height: "h-7" };
+      break;
+
+    default:
+      break;
+  }
+
+  let typeConfig = {
     icon: null,
     color: "",
   };
-
   switch (type) {
     case questionType.MULTIPLE_CHOICE:
-      config = {
-        icon: <MultipleChoiceIcon className={size} />,
+      typeConfig = {
+        icon: <MultipleChoiceIcon className={sizeConfig.iconSize} />,
         color: "bg-amber-base",
       };
       break;
 
     case questionType.POLL:
-      config = {
-        icon: <PollIcon className={size} />,
+      typeConfig = {
+        icon: <PollIcon className={sizeConfig.iconSize} />,
         color: "bg-blue-base",
       };
       break;
@@ -27,11 +45,18 @@ export default function QuestionIcon({ type, size = "w-base" }) {
     default:
       throw new Error("No match was found for the type.");
   }
+
   return (
     <div
-      className={`${config.color} flex justify-center items-center rounded w-6 h-6 text-gray-base`}
+      className={`${typeConfig.color} ${sizeConfig.width} ${sizeConfig.height}
+        flex justify-center items-center rounded text-gray-base`}
     >
-      {config.icon}
+      {typeConfig.icon}
     </div>
   );
 }
+
+QuestionIcon.propTypes = {
+  type: PropTypes.oneOf(Object.values(questionType)),
+  size: PropTypes.oneOf(["base", "sm"]),
+};
