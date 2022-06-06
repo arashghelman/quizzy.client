f7dca5554c1391956ebbbf01b3eb4a4b534f34ab
Arash Ghelman <arashghelman77@gmail.com>
2022-06-06 20:33:48 +0430

    fix: rename ProgressBar component
    ProgressBar component is renamed to ScoreProgressBar.

---
+++ src/pages/quizEditorPage/quizScoreCard/ScoreProgressBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ScoreProgressBar({ value, max, color }) {
+  return (
+    <div className={`rounded bg-gray-300`}>
+      <div
+        className={`rounded h-[13px] ${color} transform duration-300`}
+        style={{ width: value === 0 ? "0.75rem" : `${(value / max) * 100}%` }}
+      ></div>
+    </div>
+  );
+}
