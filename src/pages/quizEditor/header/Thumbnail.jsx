7fa81db92c4bf4b061d3f27e87c3324db2c0e1fe
Arash Ghelman <arashghelman77@gmail.com>
2022-06-06 20:14:38 +0430

    feat(impl): import components
    Button and EditIcon components are imported to implement changing thumbnail functionality when hovering over it.

--- src/pages/quizEditor/header/Thumbnail.jsx
+++ src/pages/quizEditor/header/Thumbnail.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@/components/Button";
+import EditIcon from "remixicon-react/PencilLineIcon";
 
 export default function Thumbnail({ url }) {
+  const [isShowingEditButton, setIsShowingEditButton] = useState(false);
   return (
-    <img
-      src={url}
-      alt="Thumbnail"
-      className="rounded-md border-1 w-[270px] h-[170px] object-cover"
-    />
+    <div
+      className="relative"
+      onMouseEnter={() => setIsShowingEditButton(true)}
+      onMouseLeave={() => setIsShowingEditButton(false)}
+    >
+      <img
+        src={url}
+        alt="Thumbnail"
+        className="border-1 rounded-md w-[320px] h-[180px] object-cover"
+      />
+      {isShowingEditButton && (
+        <Button
+          variant="flex flex-col justify-center rounded-md 
+          text-white bg-black/70 w-full h-full
+          absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        >
+          <EditIcon />
+          Choose photo
+        </Button>
+      )}
+    </div>
   );
 }
