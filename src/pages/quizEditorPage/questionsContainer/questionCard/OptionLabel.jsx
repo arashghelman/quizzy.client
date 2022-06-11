5464b9b2922b095e380477509da6245364d7a181
Arash Ghelman <arashghelman77@gmail.com>
2022-06-11 17:38:38 +0430

    fix: rename Option component
    Option component is renamed to OptionLabel.

--- src/pages/quizEditorPage/questionsContainer/questionCard/OptionLabel.jsx
+++ src/pages/quizEditorPage/questionsContainer/questionCard/OptionLabel.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Option({ color, children }) {
+export default function OptionLabel({ color, label }) {
   return (
     <div className="text-primary flex items-center gap-3">
       <span className={`${color} w-3 h-3 inline-block rounded-full`}></span>
-      {children}
+      {label}
     </div>
   );
 }
