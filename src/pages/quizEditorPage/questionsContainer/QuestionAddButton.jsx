9e9291d586c824c32cebe228c52931a5b6fbb10f
Arash Ghelman <arashghelman77@gmail.com>
2022-06-11 17:27:46 +0430

    fix: rename AddQuestionButton component
    AddQuestionButton component is renamed to QuestionAddButton.

--- src/pages/quizEditorPage/questionsContainer/QuestionAddButton.jsx
+++ src/pages/quizEditorPage/questionsContainer/QuestionAddButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AddIcon from "remixicon-react/AddLineIcon";
 import Button from "@/components/Button";
 
-export default function AddQuestionButton({ onClick }) {
+export default function QuestionAddButton({ onClick }) {
   return (
     <Button variant="text-sky-base hover:text-sky-base/70" onClick={onClick}>
       <AddIcon className="w-sm" />
