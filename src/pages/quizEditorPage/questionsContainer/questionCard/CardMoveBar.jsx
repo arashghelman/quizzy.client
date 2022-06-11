8f987e17ddde6404ae3650fd5164fe9f9c2a5ed9
Arash Ghelman <arashghelman77@gmail.com>
2022-06-11 17:36:17 +0430

    fix: rename MoveBar component
    MoveBar component is renamed to CardMoveBar.

--- src/pages/quizEditorPage/questionsContainer/questionCard/CardMoveBar.jsx
+++ src/pages/quizEditorPage/questionsContainer/questionCard/CardMoveBar.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import Button from "@/components/Button";
 import MoveIcon from "remixicon-react/DragMove2LineIcon";
 
-export default function MoveBar({ cursor, color, isHovered, onHover }) {
+export default function CardMoveBar({ cursor, color, isHovered, onHover }) {
   return (
     <div
       onMouseEnter={onHover}
       onMouseLeave={onHover}
-      className={`${color} text-gray-base grid col-span-3 justify-center items-center rounded-l-lg 
+      className={`${color} text-gray-base flex rounded-l-lg 
       w-2 hover:w-10
       transform duration-200`}
     >
       {isHovered && (
-        <>
-          <Button variant={`h-full ${cursor}`}>
-            <MoveIcon className="w-base" />
-          </Button>
-        </>
+        <Button variant={`block px-[10px] w-full h-full ${cursor}`}>
+          <MoveIcon className="w-base" />
+        </Button>
       )}
     </div>
   );
