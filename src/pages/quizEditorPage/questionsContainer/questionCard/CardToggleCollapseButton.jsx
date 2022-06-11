98c7491d321cf46bbfe3e1dbc4295f6ab6c81c3a
Arash Ghelman <arashghelman77@gmail.com>
2022-06-11 17:38:09 +0430

    fix: rename CollapseButton component
    CollapseButton component is renamed to CardToggleCollapseButton.

--- src/pages/quizEditorPage/questionsContainer/questionCard/CardToggleCollapseButton.jsx
+++ src/pages/quizEditorPage/questionsContainer/questionCard/CardToggleCollapseButton.jsx
@@ -3,7 +3,7 @@ import CollapseIcon from "remixicon-react/ArrowUpSLineIcon";
 import ExpandIcon from "remixicon-react/ArrowDownSLineIcon";
 import Button from "@/components/Button";
 
-export default function CollapseButton({ isCollapsed, onClick }) {
+export default function CardToggleCollapseButton({ isCollapsed, onClick }) {
   return (
     <Button
       variant="w-full flex justify-center py-1 text-gray-400 hover:bg-gray-100"
