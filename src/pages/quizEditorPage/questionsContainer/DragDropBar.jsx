9f7f6ac0e81c689aaa488312eabc9e0a2ffca540
Arash Ghelman <arashghelman77@gmail.com>
2022-06-11 17:45:12 +0430

    feat(impl): define a z-10 css class for container

--- src/pages/quizEditorPage/questionsContainer/DragDropBar.jsx
+++ src/pages/quizEditorPage/questionsContainer/DragDropBar.jsx
@@ -4,7 +4,7 @@ import Button from "@/components/Button";
 export default function DragDropBar({ onSave, onCancel }) {
   return (
     <div
-      className="flex justify-between w-full -translate-x-1/2 left-1/2
+      className="z-10 flex justify-between w-full -translate-x-1/2 left-1/2
         border-t-1 border-blue-light-1 py-base px-8 pb-loose fixed bottom-0 bg-blue-light-2"
     >
       <span className="text-gray-base font-semibold text-lg">
