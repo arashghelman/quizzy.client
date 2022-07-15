0458fe2d43b82dad6b93b8795e7cf0c07c5feb52
Arash Ghelman <arashghelman77@gmail.com>
2022-07-15 10:10:13 +0430

    fix: define a varinatConfig object

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,23 @@ import React from "react";
 
 export default function Button({
   type = "button",
-  variant,
+  variant = "primary",
+  customStyle,
   onClick,
   children,
   ...rest
 }) {
+  const variantConfig = {
+    primary: `text-gray-800 bg-white border-1 border-gray-300 hover:bg-gray-200`,
+    blue: `px-3 py-2 bg-blue-500 hover:bg-blue-700 text-white`,
+  };
   return (
     <button
       type={type}
       onClick={onClick}
       {...rest}
-      className={`flex items-center gap-tight ${variant}`}
+      className={`flex justify-center items-center rounded focus:ring-4 
+        ${variantConfig[variant]} ${customStyle}`}
     >
       {children}
     </button>
