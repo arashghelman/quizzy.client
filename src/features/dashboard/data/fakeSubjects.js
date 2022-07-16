bd7c05517cff2c3a4fd088495e8a05907fde1a94
Arash Ghelman <arashghelman77@gmail.com>
2022-07-16 13:49:42 +0430

    feat(impl): export subjects array

---
+++ src/features/dashboard/data/fakeSubjects.js
@@ -0,0 +1,42 @@
+export const subjects = [
+  {
+    id: "1",
+    name: "Fun",
+  },
+  {
+    id: "2",
+    name: "History",
+  },
+  {
+    id: "3",
+    name: "English",
+  },
+  {
+    id: "4",
+    name: "French",
+  },
+  {
+    id: "5",
+    name: "Languages",
+  },
+  {
+    id: "6",
+    name: "Politics",
+  },
+  {
+    id: "7",
+    name: "Geography",
+  },
+  {
+    id: "8",
+    name: "Physics",
+  },
+  {
+    id: "9",
+    name: "Chemistory",
+  },
+  {
+    id: "10",
+    name: "Social skills",
+  },
+];
