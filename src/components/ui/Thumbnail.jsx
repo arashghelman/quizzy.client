f173e287f3cb239400453142eebae7daa74ba7d8
Arash Ghelman <arashghelman77@gmail.com>
2022-07-18 15:17:29 +0430

    feat(impl): define sizeConfig

--- src/components/ui/Thumbnail.jsx
+++ src/components/ui/Thumbnail.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-export default function Thumbnail({ url }) {
+export default function Thumbnail({ size = "sm", url }) {
+  const sizeConfig = {
+    sm: "w-11 h-11",
+    md: "w-16 h-16",
+  };
   return (
-    <div className="flex rounded w-11 h-11 overflow-hidden">
+    <div className={`flex rounded overflow-hidden ${sizeConfig[size]}`}>
       <img src={url} alt="thumbnail" className="object-cover" />
     </div>
   );
