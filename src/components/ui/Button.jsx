850575728b35d65791cbcf966f5103d344e1e634
Arash Ghelman <arashghelman77@gmail.com>
2022-07-20 12:18:37 +0430

    fix: change styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -9,18 +9,22 @@ export default function Button({
 }) {
   const variantConfig = {
     outlined:
-      "px-4 py-2 rounded text-gray-800 border-1 border-gray-300 hover:bg-gray-200",
-    contained: "px-4 py-2 rounded text-white bg-blue-500 hover:bg-blue-700",
+      "justify-center gap-1 px-4 py-2 rounded text-gray-800 border-1 border-gray-300 hover:bg-gray-200",
+    contained:
+      "justify-center gap-1 px-4 py-2 rounded text-white bg-blue-500 hover:bg-blue-700",
     "modal-close":
-      "rounded-full w-7 h-7 text-gray-800 bg-gray-200 hover:bg-gray-300",
-    actions: "border-1 border-gray-300 w-8 h-8 rounded hover:bg-gray-200",
+      "justify-center rounded-full w-7 h-7 text-gray-800 bg-gray-200 hover:bg-gray-300",
+    actions:
+      "justify-center text-gray-800 border-1 border-gray-300 w-8 h-8 rounded hover:bg-gray-200",
+    "menu-item":
+      "gap-3 w-full text-gray-500 hover:text-gray-800 hover:bg-gray-100/70 rounded px-2 py-1 focus:ring-2",
   };
   return (
     <button
       type={type}
       onClick={onClick}
       {...rest}
-      className={`flex justify-center items-center gap-1
+      className={`flex items-center
       focus:outline-none focus:ring-4 ${variantConfig[variant]}`}
     >
       {children}
--- src/components/ui/Thumbnail.jsx
+++ src/components/ui/Thumbnail.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Thumbnail({ size = "sm", url }) {
   const sizeConfig = {
     sm: "w-11 h-11",
-    md: "w-16 h-16",
+    md: "w-14 h-14",
   };
   return (
     <div className={`flex rounded overflow-hidden ${sizeConfig[size]}`}>
