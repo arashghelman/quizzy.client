18ed5573a601f563b18a16817393b3a73387ce7e
Arash Ghelman <arashghelman77@gmail.com>
2022-08-09 17:28:29 +0430

    feat(impl): add elements

--- src/components/ui/Thumbnail.jsx
+++ src/components/ui/Thumbnail.jsx
@@ -4,6 +4,7 @@ export default function Thumbnail({ size = "sm", url }) {
   const sizeConfig = {
     sm: "w-12 h-12",
     md: "w-14 h-14",
+    lg: "w-fit h-40",
   };
   return (
     <div className={`flex rounded overflow-hidden ${sizeConfig[size]}`}>
--- src/features/quizManagement/components/OverviewPanel.jsx
+++ src/features/quizManagement/components/OverviewPanel.jsx
@@ -1,5 +1,40 @@
+import { quizzes } from "@/data/fakeQuizzes";
+import { quizStatus } from "@/data/quizStatus";
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Button from "@/components/ui/Button";
+import Thumbnail from "@/components/ui/Thumbnail";
 
 export default function OverviewPanel() {
-  return <div>OverviewPanel</div>;
+  const { id } = useParams();
+  const quiz = quizzes.find((quiz) => quiz.id === id);
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="grid grid-flow-col auto-cols-auto justify-between">
+        <div className="flex gap-4">
+          <Thumbnail size="lg" url={quiz.thumbnail} />
+          <div className="flex flex-col gap-4">
+            <h3 className="text-gray-800 font-bold text-xl">{quiz.name}</h3>
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-800 font-semibold">Subjects</span>
+              <span className="text-gray-500">Fun, English, Languages</span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-800 font-semibold">Status</span>
+              <span className="text-gray-500">{quizStatus[quiz.status]}</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-fit">
+          <Button variant="contained" onClick={() => navigate("../settings")}>
+            Edit info
+          </Button>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 }
