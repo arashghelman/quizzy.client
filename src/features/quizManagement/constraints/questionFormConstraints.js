27711deea2bd6341143e13487beddd22fbed8dd6
Arash Ghelman <arashghelman77@gmail.com>
2022-09-02 18:19:02 +0430

    feat(impl): add files for question form validation handling

---
+++ src/features/quizManagement/constraints/questionFormConstraints.js
@@ -0,0 +1,2 @@
+export const ANSWERS_MIN_LENGTH = 2;
+export const ANSWERS_MAX_LENGTH = 5;
--- src/features/quizManagement/data/questionFormSchema.js
+++ src/features/quizManagement/data/questionFormSchema.js
@@ -1,17 +1,41 @@
-export const schema = {
-  type: {
-    required: {
-      value: true,
-      message: "Please choose a question type",
-    },
-  },
-  title: {
-    required: {
-      value: true,
-      message: "Please enter a question title",
-    },
-  },
-  answer: {
-    value: { required: { value: true, message: "Please enter an answer" } },
-  },
-};
+import * as yup from "yup";
+import * as messages from "../messages/questionFormMessages";
+import * as constraints from "../constraints/questionFormConstraints";
+
+export const typeSchema = yup.object({
+  type: yup.string().nullable().required(messages.TYPE_REQUIRED_MESSAGE),
+});
+
+const baseAnswersSchema = yup.object({
+  title: yup.string().required(messages.TITLE_REQUIRED_MESSAGE),
+});
+
+export const multipleChoiceAnswersSchema = baseAnswersSchema.shape({
+  answers: yup
+    .array()
+    .min(constraints.ANSWERS_MIN_LENGTH, messages.ANSWERS_LENGTH_MESSAGE)
+    .max(constraints.ANSWERS_MAX_LENGTH, messages.ANSWERS_LENGTH_MESSAGE)
+    .of(
+      yup.object().shape({
+        value: yup.string().required(messages.ANSWER_REQUIRED_MESSAGE),
+        isCorrect: yup.boolean(),
+      })
+    )
+    .test(
+      "oneCorrectAnswer",
+      messages.ONE_CORRECT_ANSWER_MESSAGE,
+      (value) => value.filter((val) => val.isCorrect).length === 1
+    ),
+});
+
+export const pollAnswersSchema = baseAnswersSchema.shape({
+  answers: yup
+    .array()
+    .min(constraints.ANSWERS_MIN_LENGTH, messages.ANSWERS_LENGTH_MESSAGE)
+    .max(constraints.ANSWERS_MAX_LENGTH, messages.ANSWERS_LENGTH_MESSAGE)
+    .of(
+      yup.object().shape({
+        value: yup.string().required(messages.ANSWER_REQUIRED_MESSAGE),
+      })
+    ),
+});
