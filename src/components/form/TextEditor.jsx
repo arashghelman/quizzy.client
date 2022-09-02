44426c56fd7e58198fd997bcd84c56c1ff1d3ec8
Arash Ghelman <arashghelman77@gmail.com>
2022-09-02 19:42:56 +0430

    feat(impl): define base template and style for text editor

--- src/components/form/TextEditor.jsx
+++ src/components/form/TextEditor.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import * as TbIcons from "react-icons/tb";
+import Button from "../ui/Button";
 
 export default function TextEditor() {
-  return <div>TextEditor</div>;
+  return (
+    <div className="border-gray-300 border-1 rounded">
+      <div className="flex items-center gap-4 px-4 py-2 border-b-1 border-gray-300">
+        <Button>
+          <TbIcons.TbPhoto className="w-6 h-6 text-gray-800 hover:text-gray-800/70" />
+        </Button>
+        <Button>
+          <TbIcons.TbMath className="w-6 h-6 text-gray-800 hover:text-gray-800/70" />
+        </Button>
+        <Button>
+          <TbIcons.TbSpace className="w-6 h-6 text-gray-800 hover:text-gray-800/70" />
+        </Button>
+      </div>
+      <div
+        contentEditable
+        className="px-4 py-2 text-gray-800 focus:outline-none"
+      >
+        <span className="text-blue-500">___</span>
+        fksjfsdfkjksld
+      </div>
+    </div>
+  );
 }
