ddec78bfd9e4294be5e68cbf0b25db914c8f0a81
arashghelman <arashghelman77@gmail.com>
2023-01-06 20:42:58 +0330

    move answer control inputs to a component

--- src/features/quizManagement/components/AnswerInputGroup.jsx
+++ src/features/quizManagement/components/AnswerInputGroup.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import InputGroup from "@/components/form/InputGroup";
+import Label from "@/components/form/Label";
+import InputCheck from "@/components/form/InputCheck";
+import InputText from "@/components/form/InputText";
 import Button from "@/components/ui/Button";
-import * as HiIcons from "react-icons/hi";
+import * as FaIcons from "react-icons/fa";
 
 export default function AnswerInputGroup({
-  label,
-  inputText,
-  inputRadio,
-  onDelete,
+  index,
+  value,
+  isRemoveDisabled,
+  register,
+  onIsTrueChange,
+  onRemoveClick,
 }) {
   return (
     <InputGroup>
-      <div className="flex items-center justify-between">
-        {label}
-        <div className="flex items-center gap-2">
-          {inputRadio}
-          <Button variant="basic" onClick={onDelete}>
-            <HiIcons.HiTrash className="text-red-500 w-5 h-5" />
+      <div className="flex items-center justify-between gap-2">
+        <Label id={`answers[${index}].value`}>{`Answer ${index + 1}`}</Label>
+        <div className="flex gap-2">
+          <InputCheck
+            type="radio"
+            name="answers"
+            value={value}
+            onChange={onIsTrueChange}
+          />
+          <Button
+            variant="hover:opacity-70"
+            onClick={onRemoveClick}
+            disabled={isRemoveDisabled}
+          >
+            <FaIcons.FaTrashAlt className="text-red-500 w-4 h-4" />
           </Button>
         </div>
       </div>
-      {inputText}
+      <InputText
+        id={`answers[${index}].value`}
+        placeholder="Type your answer here..."
+        register={() => register(`answers[${index}].value`)}
+      />
     </InputGroup>
   );
 }
--- src/features/quizManagement/components/QuestionForm.jsx
+++ src/features/quizManagement/components/QuestionForm.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import InputGroup from "@/components/form/InputGroup";
+// import InputGroup from "@/components/form/InputGroup";
 // import ErrorText from "@/components/form/ErrorText";
-import AnswersFieldGroup from "./AnswersFieldGroup";
 import Button from "@/components/ui/Button";
 // import { schema } from "../validations/questionFormSchema";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 // import { yupResolver } from "@hookform/resolvers/yup";
 import { EditorState } from "draft-js";
 import TextEditor from "@/components/form/TextEditor";
+import AnswerInputGroup from "./AnswerInputGroup";
+import * as RiIcons from "react-icons/ri";
 
 export default function QuestionForm() {
   const {
@@ -15,7 +16,7 @@ export default function QuestionForm() {
     control,
     getValues,
     setValue,
-    formState: { errors },
+    // formState: { errors },
     handleSubmit,
   } = useForm({
     defaultValues: {
@@ -29,27 +30,53 @@ export default function QuestionForm() {
     // resolver: yupResolver(schema),
   });
 
+  const { fields, append, remove, update } = useFieldArray({
+    control,
+    name: "answers",
+  });
+
   return (
     <form
       method="dialog"
       onSubmit={handleSubmit((data) => console.log(data))}
       className="flex flex-col gap-4 w-full"
     >
-      <InputGroup>
-        <TextEditor
-          name="content"
-          control={control}
-          setValue={setValue}
-          placeholder="Type your question here"
-        />
-      </InputGroup>
-      <AnswersFieldGroup
+      <TextEditor
+        name="content"
         control={control}
-        register={register}
-        errors={errors}
-        getValues={getValues}
         setValue={setValue}
+        placeholder="Type your question here"
       />
+      <div className="grid gap-4">
+        {fields.map((field, index) => (
+          <AnswerInputGroup
+            key={index}
+            index={index}
+            value={field.value}
+            isRemoveDisabled={fields.length === 2}
+            register={register}
+            onRemoveClick={() => remove(index)}
+            onIsTrueChange={() =>
+              getValues("answers").forEach((answer, i) =>
+                update(i, {
+                  ...answer,
+                  isCorrect: i === index ? true : false,
+                })
+              )
+            }
+          />
+        ))}
+      </div>
+      {fields.length < 5 && (
+        <div className="self-center w-full">
+          <Button
+            variant="w-full flex justify-center p-4 mt-2 border-1 border-dashed border-gray-300 hover:border-gray-500 text-gray-400 rounded group"
+            onClick={() => append({ value: "", isCorrect: false })}
+          >
+            <RiIcons.RiAddFill className="w-5 h-5 group-hover:text-gray-500" />
+          </Button>
+        </div>
+      )}
       <div className="flex gap-2 self-end">
         <Button variant="ButtonOutlined">Cancel</Button>
         <Button variant="ButtonContained" type="submit">
